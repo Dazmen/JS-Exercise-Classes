e6b6a6e976ffd3977fc0197c6cb1d0793c993ab4
Chris Engel <Chris.Engel@yahoo.com>
2019-11-14 15:27:12 -0500

    task 2 complete

--- index.js
+++ index.js
@@ -74,7 +74,26 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    if(this.tank * this.milesPerGallon >= distance){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    }
+    else{
+      this.odometer = this.odometer + (this.tank * this.milesPerGallon);
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
